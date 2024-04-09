5ed66e4cc9ff2aba7c3ac6a770a676e3b9841703
Eli Clemente Gordillo Foster <elifosterwy@gmail.com>
2024-04-09 12:20:47 -0700

    Update to 1.20.4 NeoForge. Close #14.

--- src/main/java/com/gamepedia/ftb/defaultskin/DefaultSkinMod.java
+++ src/main/java/com/gamepedia/ftb/defaultskin/DefaultSkinMod.java
@@ -1,15 +1,14 @@
 package com.gamepedia.ftb.defaultskin;
 
 import net.minecraft.client.KeyMapping;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import net.neoforged.neoforge.event.TickEvent;
 import org.lwjgl.glfw.GLFW;
 
 @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = "defaultskin")
@@ -18,9 +17,10 @@ public class DefaultSkinMod {
     private static final KeyMapping key = new KeyMapping("key.defaultskin.desc", GLFW.GLFW_KEY_RIGHT_BRACKET, "key.defaultskin.category");
     private static boolean isEnabled = false;
 
-    public DefaultSkinMod() {
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> (DistExecutor.SafeRunnable) () -> bus.addListener(this::registerKeyBinding));
+    public DefaultSkinMod(IEventBus bus) {
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            bus.addListener(this::registerKeyBinding);
+        }
     }
 
     public void registerKeyBinding(RegisterKeyMappingsEvent event) {
--- src/main/java/com/gamepedia/ftb/defaultskin/mixins/AbstractClientPlayerMixin.java
+++ src/main/java/com/gamepedia/ftb/defaultskin/mixins/AbstractClientPlayerMixin.java
@@ -4,8 +4,8 @@ import com.gamepedia.ftb.defaultskin.DefaultSkinMod;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.resources.DefaultPlayerSkin;
+import net.minecraft.client.resources.PlayerSkin;
 import net.minecraft.core.BlockPos;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,10 +19,10 @@ public abstract class AbstractClientPlayerMixin extends Player {
         super(level, pos, yRot, profile);
     }
 
-    @Inject(method = "getSkinTextureLocation", at = @At("HEAD"), cancellable = true)
-    private void defaultskin$getTextureLocation(CallbackInfoReturnable<ResourceLocation> info) {
+    @Inject(method = "getSkin", at = @At("HEAD"), cancellable = true)
+    private void defaultskin$getTextureLocation(CallbackInfoReturnable<PlayerSkin> info) {
         if (DefaultSkinMod.isEnabled()) {
-            info.setReturnValue(DefaultPlayerSkin.getDefaultSkin(getUUID()));
+            info.setReturnValue(DefaultPlayerSkin.get(getUUID()));
         }
     }
 }
